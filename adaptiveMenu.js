08ff593046cc3d06a6c7230f521c130cddba77da
Roman-Green <71728007+Roman-Green@users.noreply.github.com>
2021-02-19 17:23:02 +0200

    v 2.0

--- adaptiveMenu.js
+++ adaptiveMenu.js
@@ -2,5 +2,5 @@ let burgerMenu = document.querySelector(".burger-menu");
 let adaptiveMenu = document.querySelector(".header-menu-navigation_adaptive");
 
 burgerMenu.onclick = function() {
-    adaptiveMenu.classList.toggle("hiden");
+    adaptiveMenu.classList.toggle("hidden");
 }
